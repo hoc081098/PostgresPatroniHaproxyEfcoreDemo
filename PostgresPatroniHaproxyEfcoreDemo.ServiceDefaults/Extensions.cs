186b57f60ae76eedf2a3fc6c135ddc1b19c1bd04
hoc081098@gmail.com <hoc081098@gmail.com>
2026-02-26 17:12:12 +0700

    Add OpenTelemetry instrumentation for Entity Framework Core and Npgsql

--- PostgresPatroniHaproxyEfcoreDemo.ServiceDefaults/Extensions.cs
+++ PostgresPatroniHaproxyEfcoreDemo.ServiceDefaults/Extensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ServiceDiscovery;
+using Npgsql;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
@@ -70,7 +71,9 @@ public static class Extensions
                     )
                     // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
                     //.AddGrpcClientInstrumentation()
-                    .AddHttpClientInstrumentation();
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddNpgsql();
             });
 
         builder.AddOpenTelemetryExporters();
